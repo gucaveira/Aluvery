0f6f66bb73a74334fdd809cc2de857fa5ff4d0e2
gustavo rocha <pereiragustavo215@gmail.com>
2023-02-09 11:52:57 -0300

    modificando design dos Composables

--- app/src/main/java/com/gustavo/aluvery/MainActivity.kt
+++ app/src/main/java/com/gustavo/aluvery/MainActivity.kt
@@ -4,10 +4,20 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gustavo.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +28,56 @@ class MainActivity : ComponentActivity() {
                 Surface {
                     MyFirstComposable()
                 }
+            }
+        }
+    }
+}
+
+// a orde do Modifier importa
+@Preview(showBackground = true)
+@Composable
+fun CustomLayoutPreview() {
+    Column(
+        Modifier
+            .padding(all = 8.dp)
+            .background(color = Color.Blue)
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Text(text = "text 1")
+        Text(text = "text 2")
+
+        Row(
+            Modifier
+                .padding(all = 8.dp)
+                .background(color = Color.Green)
+                .fillMaxHeight(0.1f)
+        ) {
+            Text(text = "text 3")
+            Text(text = "text 4")
+        }
+
+        Box(
+            Modifier
+                .padding(all = 8.dp)
+                .background(color = Color.Red)
+        ) {
+            Row(
+                Modifier
+                    .padding(all = 8.dp)
+                    .background(color = Color.Cyan)
+            ) {
+                Text(text = "text 5")
+                Text(text = "text 6")
+            }
+
+            Column(
+                Modifier
+                    .padding(all = 8.dp)
+                    .background(color = Color.Yellow)
+            ) {
+                Text(text = "Texto 7")
+                Text(text = "Texto 8")
             }
         }
     }
@@ -38,6 +98,7 @@ fun MyFirstComposable() {
     widthDp = 300,
     showBackground = true
 )
+
 @Composable
 fun MyFirstComposablePreview() {
     AluveryTheme {
