86467db4c063afa432acef1353cd88f819c9bd93
gustavo rocha <pereiragustavo215@gmail.com>
2023-02-14 09:34:23 -0300

    add click e Ellipsis ao texto

--- app/src/main/java/com/gustavo/aluvery/ui/components/CardProductItem.kt
+++ app/src/main/java/com/gustavo/aluvery/ui/components/CardProductItem.kt
@@ -1,6 +1,7 @@
 package com.gustavo.aluvery.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,31 +12,43 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.gustavo.aluvery.R
 import com.gustavo.aluvery.extensions.toBrazilianCurrency
 import com.gustavo.aluvery.model.Product
-import com.gustavo.aluvery.sampledata.sampleProducts
 import com.gustavo.aluvery.ui.theme.AluveryTheme
+import java.math.BigDecimal
 
 @Composable
 fun CardProductItem(
     product: Product,
     modifier: Modifier = Modifier,
-    elevation: Dp = 4.dp
+    elevation: Dp = 4.dp,
+    expanded: Boolean = false
 ) {
+    var expandedState by remember { mutableStateOf(expanded) }
+
     // o modifier recebido como paramentro so pode esta no compose mae.
     // Por que o tamanho global desse objeto vai ser gerenciado por quem chama ele.
     Card(
         modifier
             .fillMaxWidth()
-            .heightIn(150.dp),
+            .heightIn(150.dp)
+            .clickable {
+                expandedState = expandedState.not()
+            },
         elevation = elevation
     ) {
         Column {
@@ -61,10 +74,14 @@ fun CardProductItem(
                     text = product.price.toBrazilianCurrency()
                 )
             }
+            val textOverflow = if (expandedState) TextOverflow.Visible else TextOverflow.Ellipsis
+            val maxLines = if (expandedState) Int.MAX_VALUE else 2
             product.description?.let {
                 Text(
                     text = it,
-                    Modifier.padding(16.dp)
+                    Modifier.padding(16.dp),
+                    overflow = textOverflow,
+                    maxLines = maxLines
                 )
             }
         }
@@ -77,7 +94,43 @@ private fun CardProductItemPreview() {
     AluveryTheme {
         Surface {
             CardProductItem(
-                product = sampleProducts.random(),
+                product = Product(
+                    name = "teste",
+                    price = BigDecimal("9.99")
+                ),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CardProductItemWithDescriptionPreview() {
+    AluveryTheme {
+        Surface {
+            CardProductItem(
+                product = Product(
+                    "teste",
+                    BigDecimal("9.99"),
+                    description = LoremIpsum(10).values.first(),
+                ),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CardProductItemWithDescriptionExpandedPreview() {
+    AluveryTheme {
+        Surface {
+            CardProductItem(
+                product = Product(
+                    "teste",
+                    BigDecimal("9.99"),
+                    description = LoremIpsum(50).values.first(),
+                ),
+                expanded = true,
             )
         }
     }
