89e86732b6b9f070e062630861a8f9ed22dac6ba
gustavo rocha <pereiragustavo215@gmail.com>
2023-02-15 20:02:15 -0300

    implementado fabButton do material design e suas caracteristicas

--- app/src/main/java/com/gustavo/aluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/gustavo/aluvery/ui/activities/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.gustavo.aluvery.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.gustavo.aluvery.sampledata.sampleSections
 import com.gustavo.aluvery.ui.screens.HomeScreen
 import com.gustavo.aluvery.ui.theme.AluveryTheme
@@ -13,16 +23,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            App {
+                startActivity(Intent(this, ProductFormActivity::class.java))
+            }
         }
     }
 }
 
 @Composable
-fun App() {
+fun App(onFabClick: () -> Unit = {}) {
     AluveryTheme {
-        Surface {
-            HomeScreen(sampleSections)
+        Scaffold(floatingActionButton = { SetFloatingActionButton(onFabClick) }) { paddingValues ->
+            Box(modifier = Modifier.padding(paddingValues)) {
+                Surface {
+                    HomeScreen(sampleSections)
+                }
+            }
         }
     }
+}
+
+@Composable
+fun SetFloatingActionButton(onFabClick: () -> Unit = {}) {
+    FloatingActionButton(onClick = { onFabClick.invoke() }) {
+        Icon(imageVector = Icons.Default.Add, contentDescription = null)
+    }
+}
+
+@Preview
+@Composable
+private fun AppPreview() {
+    App()
 }
