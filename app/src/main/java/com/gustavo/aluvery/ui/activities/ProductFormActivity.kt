9c92ddac5173f3bd0d525622a92ce77d4183ebef
gustavo rocha <pereiragustavo215@gmail.com>
2023-02-15 18:49:59 -0300

    add imagem ao form

--- app/src/main/java/com/gustavo/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/gustavo/aluvery/ui/activities/ProductFormActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
@@ -20,9 +21,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.gustavo.aluvery.R
 import com.gustavo.aluvery.model.Product
 import com.gustavo.aluvery.ui.theme.AluveryTheme
 import java.math.BigDecimal
@@ -57,6 +62,19 @@ fun ProductFormScreen() {
         )
 
         var url by remember { mutableStateOf("") }
+
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
+        }
 
         TextField(
             value = url,
