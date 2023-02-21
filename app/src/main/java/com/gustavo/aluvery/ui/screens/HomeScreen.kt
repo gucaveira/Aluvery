79c834136a7bf36d413ebd3df52ab1de0fb761a9
gustavo rocha <pereiragustavo215@gmail.com>
2023-02-21 20:17:57 -0300

    finalizando a aplicacao no HomeScreenViewModel

--- app/src/main/java/com/gustavo/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/gustavo/aluvery/ui/screens/HomeScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,7 +55,7 @@ fun HomeScreen(state: HomeScreenUiState = HomeScreenUiState()) {
 
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel) {
-    val state = viewModel.uiState
+    val state by viewModel.uiState.collectAsState()
     HomeScreen(state = state)
 }
 
--- app/src/main/java/com/gustavo/aluvery/ui/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/com/gustavo/aluvery/ui/viewmodels/HomeScreenViewModel.kt
@@ -1,8 +1,5 @@
 package com.gustavo.aluvery.ui.viewmodels
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gustavo.aluvery.dao.ProductDao
@@ -11,30 +8,40 @@ import com.gustavo.aluvery.sampledata.sampleCandies
 import com.gustavo.aluvery.sampledata.sampleDrinks
 import com.gustavo.aluvery.sampledata.sampleProducts
 import com.gustavo.aluvery.ui.uiState.HomeScreenUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class HomeScreenViewModel : ViewModel() {
 
     private val dao = ProductDao()
 
-    var uiState: HomeScreenUiState by mutableStateOf(HomeScreenUiState(
-        onSearchChanger = {
-            uiState = uiState.copy(searchText = it, searchedProduct = searchedProduct(it))
-        }
-
-    ))
-        private set
+    private val _uiState = MutableStateFlow(HomeScreenUiState())
+    val uiState get() = _uiState.asStateFlow()
 
     init {
+        _uiState.update { currentState ->
+            currentState.copy(onSearchChanger = {
+
+                _uiState.value = _uiState.value.copy(
+                    searchText = it,
+                    searchedProduct = searchedProduct(it)
+                )
+            })
+        }
+
+
         viewModelScope.launch {
             dao.products().collect { products ->
-                uiState = uiState.copy(
+                _uiState.value = _uiState.value.copy(
                     sections = mapOf(
                         "Todos produtos" to products,
                         "Promoções" to sampleDrinks + sampleCandies,
                         "Doces" to sampleCandies,
                         "Bebidas" to sampleDrinks
-                    )
+                    ),
+                    searchedProduct = searchedProduct(_uiState.value.searchText)
                 )
             }
         }
